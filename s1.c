82b2865f61116fcb8c59368b7b62d88d108d3a58
zhangertong <zhangertong@gmail.com>
2014-01-09 15:47:14 -0600

    second commit

---
+++ s1.c
@@ -0,0 +1,117 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <linux/net_tstamp.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUFLEN 1542
+#define PORT 9933
+#define PORT1 9932
+ 
+void err(char *str)
+{
+    perror(str);
+    exit(1);
+}
+
+double receive( )
+{
+    struct sockaddr_in my_addr, cli_addr;
+    int sockfd, i;
+    double r;
+    socklen_t slen;
+    
+    char buf[BUFLEN]; 
+    slen=sizeof(cli_addr);
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+      err("socket");
+    else
+      printf("Server : Socket() successful\n");
+ 
+    bzero(&my_addr, sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(PORT1);
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+     
+    if (bind(sockfd, (struct sockaddr* ) &my_addr, sizeof(my_addr))==-1)
+      err("bind");
+    printf("Server : bind() successful\n");
+    i=recvfrom(sockfd, buf, BUFLEN, 0, (struct sockaddr*)&cli_addr, &slen);
+    if(i<0)
+            err("recvfrom()");
+	sscanf(buf, "%lf", &r);
+     printf("%lf\n",r);
+    close(sockfd);
+    return r;
+}
+ 
+int main(int argc, char* argv[])
+{
+     
+    struct sockaddr_in serv_addr;
+    int sockfd, i, slen=sizeof(serv_addr);
+    char buf[BUFLEN];
+    struct timeval t1, t2;
+    int t=100;
+    if(argc<2)
+    {
+      printf("./client ip\n");
+      return -1;
+    }
+    if(argc>2) t=atoi(argv[2]);
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+        err("socket");
+    int timestamp_flags=SOF_TIMESTAMPING_TX_SOFTWARE;
+    if(setsockopt(sockfd, SOL_SOCKET,SO_TIMESTAMPING,&timestamp_flags,sizeof(timestamp_flags))<0) {
+    err("timestamp error");
+    } 
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    if (inet_aton(argv[1], &serv_addr.sin_addr)==0)
+    {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
+   double delay=0; 
+   int id=0;
+   int jj;
+   printf("jj=%d******************\n",jj);
+  for( i=0;i<t;i++) 
+	{
+	    gettimeofday(&t1,NULL);
+	    sprintf(buf, "%d,%ld,%ld\n",id, t1.tv_sec,t1.tv_usec);
+	    id++;
+            int buffersize=1450;
+	    if (sendto(sockfd, buf, buffersize, 0,                  (struct sockaddr*)&serv_addr, slen)==-1)
+		err("sendto()");
+	    long dif=0;
+	    delay=160;
+	    while(dif<delay)
+	    {
+		gettimeofday(&t2,NULL);
+		dif=(t2.tv_sec-t1.tv_sec)*1000000+(t2.tv_usec-t1.tv_usec);
+	    } 
+	}
+
+	if(strcpy(buf,"exit\0") == 0) return -1;
+ i =5;
+while (i>3) {
+	if (sendto(sockfd, buf, 20, 0, (struct sockaddr*)&serv_addr, slen)==-1)
+		err("sendto()");
+	double r=2;
+        sleep(1);
+i--;
+}
+
+/*        while (r>1)
+	{
+	  r=receive();
+	}*/ 
+    return 0;
+}
